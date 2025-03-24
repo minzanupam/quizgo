d206489a4a36696ce9487dbf266b944e715d93f3
anupam <anupamminz300am@gmail.com>
2025-03-24 21:10:14 +0530

    connect to database

--- src/main.go
+++ src/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +25,15 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	mux := http.NewServeMux()
+	log.Println(conn)
 
 	mux.HandleFunc("GET /", rootHandler)
 
