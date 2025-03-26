b2f90c7b0d4b4517fa422425740cc6e13c1b96e0
anupam <anupamminz300am@gmail.com>
2025-03-26 11:55:22 +0530

    display quiz data

--- src/services/quiz.go
+++ src/services/quiz.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"quizgo/src/views"
 	"strconv"
+	"time"
 )
 
 func (s *Service) quizParentPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,7 +45,32 @@ func (s *Service) quizApiHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Service) quizPageHandler(w http.ResponseWriter, r *http.Request) {
-	page := views.QuizPage()
+	userID, err := authenticate(s.Store, r)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	quizID, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	row := s.Db.QueryRow(r.Context(), `SELECT title, created_at, updated_at
+		FROM quizzes WHERE ID = $1 AND owner_id = $2`, quizID, userID)
+
+	var quiz views.DBQuiz
+	var createdAt time.Time
+	var updatedAt time.Time
+	if err = row.Scan(&quiz.Title, &createdAt, &updatedAt); err != nil {
+		log.Println(err)
+	}
+	quiz.ID = strconv.Itoa(quizID)
+	quiz.CreatedAt = createdAt.Format(time.RFC3339)
+	quiz.UpdatedAt = updatedAt.Format(time.RFC3339)
+
+	page := views.QuizPage(quiz)
 	if err := page.Render(r.Context(), w); err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
